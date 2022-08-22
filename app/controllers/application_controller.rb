6e69d0f3801e5f630f99938478a038bb53dbcb0d
Noah Licata <noahinbldr@gmail.com>
2022-08-22 16:13:49 -0600

    added application methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,41 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, "application/json"
 
+  # get all clothes
   get "/clothes" do
-    { message: "some clothes" }.to_json
+    clothings = Clothing.all
+    clothing.to_json
+  end
+
+  # get one single piece of clothing
+  get "/clothes/:id" do
+    clothing = Clothing.find(params[:id])
+    clothing.to_json
+  end
+
+  # delete review
+  delete "/reviews/:id" do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
+  #post review
+  post "/reviews" do
+    review = Review.create(
+      comment: params[:comment],
+      user_id: params[:user_id],
+      clothing_id: params[:clothing_id],
+    )
+    review.to_json
+  end
+
+  #patch review
+  patch "/reviews/:id" do
+    review = Review.find(params[:id])
+    review.update(
+      comment: params[:comment],
+    )
+    review.to_json
   end
 end
