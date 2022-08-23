798bcab332328d422d6ba23e267cd036e1a5ba57
Noah Licata <noahinbldr@gmail.com>
2022-08-23 09:19:41 -0600

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,56 @@
 puts "🌱 Seeding Clothing..."
 
-Clothing.create(title: "Shirt 1", price: 5, quantity: 1, brand: "some brand")
-Clothing.create(title: "Shirt 2", price: 6, quantity: 1, brand: "some other brand")
-Clothing.create(title: "Shirt 3", price: 7, quantity: 1, brand: "some other other brand")
-Clothing.create(title: "Shirt 4", price: 8, quantity: 1, brand: "some other other other brand")
+clothing_1 = Clothing.create(title: "AAUJW0083OT02GRY0002 HERO 4X CHAIN NECKLACE", price: 426, quantity: 5, brand: "ALYX", url: "https://cdn.shopify.com/s/files/1/2138/3643/products/AAUJW0083OT02_GRY0002_S21_1_91688b2c-7819-42cd-8f5e-4d3e47877b70_1024x.jpg?v=1638461488")
+clothing_2 = Clothing.create(title: "AAMPA0271FA03MTY0001 5 POCKET TREATED SKINNY JEAN", price: 676, quantity: 30, brand: "ALYX", url: "https://cdn.shopify.com/s/files/1/2138/3643/products/AAMOU0279LE02_WTH0004_S22_1_0e1cce93-987e-4a7c-86b4-f7aeb59205be_1024x.jpg?v=1644498928")
+clothing_3 = Clothing.create(title: "AZMOU0117FA05BLU0004 DENIM JACKET BLACKMEANS", price: 1690, quantity: 1, brand: "ALYX", url: "https://cdn.shopify.com/s/files/1/2138/3643/products/AZMOU0117FA05_BLU0004_F20_1_f0384b1a-3ae9-4ceb-9b30-37ea68bc617d_1024x.jpg?v=1658329726")
+clothing_4 = Clothing.create(title: "AAUJW0032OT01GLD0003 1017 ALYX 9SM BUCKLE NECKLACE", price: 477, quantity: 7, brand: "ALYX", url: "https://cdn.shopify.com/s/files/1/2138/3643/products/AAUJW0032OT01_GLD0003_S21_1_10208b96-6876-4e58-9dc0-500de1714312_1024x.jpg?v=1638525485")
+clothing_5 = Clothing.create(title: "AAUJW0033OT01NDE0125 TRANSPARENT CHAIN NECKLACE", price: 229, quantity: 3, brand: "ALYX", url: "https://cdn.shopify.com/s/files/1/2138/3643/products/AAUJW0033OT01_NDE0125_1_4dfa028e-12f3-415a-9ad1-07f50152dd8d_1024x.jpg?v=1638890075")
+clothing_6 = Clothing.create(title: "AAUJW0170OT01GRY0002 A CHARM NECKLACE", price: 415, quantity: 4, brand: "ALYX", url: "https://cdn.shopify.com/s/files/1/2138/3643/products/AXUJW0046OT01_GRY0002_1_5171f527-4b3a-4347-8e18-158fc78ae325_1024x.jpg?v=1629291590")
+clothing_7 = Clothing.create(title: "AAUJW0110OT01MTY0001 FADED CHAINLINK BUCKLE NECKLACE", price: 457, quantity: 6, brand: "ALYX", url: "https://cdn.shopify.com/s/files/1/2138/3643/products/AolUYrDpb3Dbk4nkQlhqP05KsgSe7KBcMEufM0-gXUsqen5Jde0cPhcV2veaSPCmxBW2KkI8mCs6KwcTpqzubJGvgDDj878Qo3JAKW38TzEFrVGEvKsR87rctTMn92vxyCltjjkgkfxvOQPDkJlsuGkcAxVdROKmZ7SlCMpr_JVxTKgiJIpsCnW_1024x.jpg?v=1624630731")
+clothing_8 = Clothing.create(title: "AAUJW0072OT03GRY0002 DUAL CHUNKY CHAIN NECKLACE", price: 332, quantity: 6, brand: "ALYX", url: "https://cdn.shopify.com/s/files/1/2138/3643/products/AAUJW0072OT03_GRY0002_F21_1_1024x.jpg?v=1622533984")
+clothing_9 = Clothing.create(title: "AAUJW0012OT05GLD0003 ROLLERCOSTER BRACELET", price: 830, quantity: 55, brand: "ALYX", url: "https://cdn.shopify.com/s/files/1/2138/3643/products/AAUJW0012OT05_GLD0003_S21_1_a197285a-f43e-4762-9d60-5c58305836e5_1024x.jpg?v=1613206238")
+clothing_10 = Clothing.create(title: "AAUJW0012OT03GRY0011 ROLLERCOSTER BRACELET", price: 830, quantity: 4, brand: "ALYX", url: "https://cdn.shopify.com/s/files/1/2138/3643/products/AAUJW0012OT03_GRY0011_S21_1_cfbd3bbd-c62a-4865-bb3a-aa2ecd69a965_1024x.jpg?v=1613206232")
+clothing_11 = Clothing.create(title: "AAUJW0117OT01GRY0002 METAL BEADED BRACELET", price: 260, quantity: 3, brand: "ALYX", url: "https://cdn.shopify.com/s/files/1/2138/3643/products/ApMj_rtlXDKYH-ZuaT6UzNRLcSpFhXz_jEq8N0TWsCn1AgiaE58532S0hk1VXJ0aGE4fytioYx9ZUsKl9S1YR0xZnB3Y8FFrFenzl4jtYbvweA53T-UwI3wLMaMyyaUNxCIfezvHktwZ9_ih7SbM5MlLyfldHcB0j5G6N_ldSdN2qx8r3sFtm5r_1024x.jpg?v=1629282491")
+clothing_12 = Clothing.create(title: "AAUJW0171OT01GRY0002 A CHARM BRACELET", price: 260, quantity: 2, brand: "ALYX", url: "https://cdn.shopify.com/s/files/1/2138/3643/products/AAUJW0171OT01_GRY0002_F21_1_1024x.jpg?v=1629294152")
+clothing_13 = Clothing.create(title: "THE MULTI-PRINT TOP", price: 420, quantity: 500, brand: "Jean-PG", url: "https://images.prismic.io/jpg-fashion-production/00c8d0a0-3a1b-49ba-b407-93925d39503c_22-06-F-TO019-T516_1.jpg?auto=compress,format&fit=max&cs=tinysrgb&w=1440&q=55")
+clothing_14 = Clothing.create(title: "THE BANKNOTE BIKINI TOP", price: 420, quantity: 3, brand: "Jean-PG", url: "https://images.prismic.io/jpg-fashion-production/c80c17c1-e5fc-4855-8382-2e2ab7442017_22-06-F-SG006-J510_1.jpg?rect=770,280,2230,3318&auto=compress,format&fit=max&cs=tinysrgb&w=2048&q=55")
+clothing_15 = Clothing.create(title: "THE BANKNOTE BOLERO", price: 190, quantity: 45, brand: "Jean-PG", url: "https://images.prismic.io/jpg-fashion-production/2d5a21fe-0f90-4e24-8180-66f75c7fada7_22-06-U-CD004-T517_5.jpg?auto=compress,format&fit=max&cs=tinysrgb&w=1440&q=55")
+clothing_16 = Clothing.create(title: "THE BANKNOTE SARONG DRESS", price: 420, quantity: 35, brand: "Jean-PG", url: "https://images.prismic.io/jpg-fashion-production/78ad0016-bbf3-44d5-9da8-3a1ab4f5eea0_22-06-F-RO031-T517_1.jpg?auto=compress,format&fit=max&cs=tinysrgb&w=2048&q=55")
+clothing_17 = Clothing.create(title: "THE CARTOUCHE CROP TOP", price: 290, quantity: 56, brand: "Jean-PG", url: "https://images.prismic.io/jpg-fashion-production/5d1f3c4b-a72a-4919-9748-d5edca271315_22-06-U-TO018-T515_3.jpg?auto=compress,format&fit=max&cs=tinysrgb&w=1440&q=55")
+clothing_18 = Clothing.create(title: "THE CARTOUCHE BIKE SHORTS", price: 220, quantity: 232, brand: "Jean-PG", url: "https://images.prismic.io/jpg-fashion-production/ad8b2adc-a83e-4e45-bcde-9b354bbaff2e_22-06-F-SH003-T515_1.jpg?auto=compress,format&fit=max&cs=tinysrgb&w=1440&q=55")
+clothing_19 = Clothing.create(title: "THE BANKNOTE SUMMER SHIRT", price: 390, quantity: 323, brand: "Jean-PG", url: "https://images.prismic.io/jpg-fashion-production/4bb90958-58fc-498a-9bd0-642e57b89b57_22-06-U-CH005-T517_1.jpg?rect=872,165,2161,3215&auto=compress,format&fit=max&cs=tinysrgb&w=1440&q=55")
+clothing_20 = Clothing.create(title: "THE BLACK ÉVIDEMMENT TANK TOP", price: 150, quantity: 2, brand: "Jean-PG", url: "https://images.prismic.io/jpg-fashion-production/739b5478-e164-4715-9f93-85df65b0030d_22-06-U-TO032I-J013_00_2.jpg?auto=compress,format&fit=max&cs=tinysrgb&w=2048&q=55")
+clothing_21 = Clothing.create(title: "THE PINK ÉVIDEMMENT TANK TOP", price: 150, quantity: 34, brand: "Jean-PG", url: "https://images.prismic.io/jpg-fashion-production/df7dbb72-29cb-4f84-b02b-869ddf2058d5_22-06-U-TO032I-J013_21_2.jpg?auto=compress,format&fit=max&cs=tinysrgb&w=2048&q=55")
+puts "Done Seeding Clothing"
+
+puts "Seeding Users!!"
+user_1 = User.create(name: "Benny The Butcher", comment: "I've been selling dreams to sleepers.")
+user_2 = User.create(name: "Avril Lavigne", comment: "He was a skater boi.")
+user_3 = User.create(name: "Selena", comment: "Cada vez cada vez")
+user_4 = User.create(name: "New Edition", comment: "Cool it NOWWWWWWWWW, SLOWWWW it down.")
+user_5 = User.create(name: "Hammerbaby9", comment: "*snaps* My man.")
+
+puts "Done Seeding Users!!!!!!"
+
+puts "Seeding reviews..."
+Review.create(comment: "As she walked along the street and looked in the gutter, she realized facemasks had become the new cigarette butts.", user_id: user_1.id, clothing_id: clothing_1.id)
+Review.create(comment: "He decided to live his life by the big beats manifesto.", user_id: user_2.id, clothing_id: clothing_2.id)
+Review.create(comment: "It was at that moment that he learned there are certain parts of the body that you should never Nair.", user_id: user_3.id, clothing_id: clothing_3.id)
+Review.create(comment: "On a scale from one to ten, what's your favorite flavor of random grammar?", user_id: user_4.id, clothing_id: clothing_4.id)
+Review.create(comment: "The complicated school homework left the parents trying to help their kids quite confused.", user_id: user_5.id, clothing_id: clothing_5.id)
+Review.create(comment: "I would be delighted if the sea were full of cucumber juice.", user_id: user_4.id, clothing_id: clothing_6.id)
+Review.create(comment: "Please tell me you don't work in a morgue.", user_id: user_2.id, clothing_id: clothing_7.id)
+Review.create(comment: "Despite what your teacher may have told you, there is a wrong way to wield a lasso.", user_id: user_2.id, clothing_id: clothing_8.id)
+Review.create(comment: "Just go ahead and press that button.", user_id: user_1.id, clothing_id: clothing_9.id)
+Review.create(comment: "As he waited for the shower to warm, he noticed that he could hear water change temperature.", user_id: user_1.id, clothing_id: clothing_10.id)
+Review.create(comment: "The bird had a belief that it was really a groundhog.", user_id: user_4.id, clothing_id: clothing_11.id)
+Review.create(comment: "It's never comforting to know that your fate depends on something as unpredictable as the popping of corn.", user_id: user_5.id, clothing_id: clothing_12.id)
+Review.create(comment: "I come from a tribe of head-hunters, so I will never need a shrink.", user_id: user_3.id, clothing_id: clothing_13.id)
+Review.create(comment: "The glacier came alive as the climbers hiked closer.", user_id: user_4.id, clothing_id: clothing_14.id)
+Review.create(comment: "They decided to plant an orchard of cotton candy.", user_id: user_4.id, clothing_id: clothing_15.id)
+Review.create(comment: "He found the end of the rainbow and was surprised at what he found there.", user_id: user_1.id, clothing_id: clothing_16.id)
+Review.create(comment: "The hummingbird's wings blurred while it eagerly sipped the sugar water from the feeder.", user_id: user_5.id, clothing_id: clothing_17.id)
+Review.create(comment: "You've been eyeing me all day and waiting for your move like a lion stalking a gazelle in a savannah.", user_id: user_3.id, clothing_id: clothing_18.id)
+puts "Done Seeding Reviews!"
 
 puts "✅ Done seeding!"
